15c524bf10e527c11224e037ed5ee75305194add
Michael Pingleton <mnpingleton@gmail.com>
2025-03-30 11:19:58 -0400

    Initial header files.

---
+++ include/cpptest.h
@@ -0,0 +1,33 @@
+//-----------------------------------------------------------------------------
+// CPPTEST
+//
+// cpptest.h
+//
+// Primary include header file for the CPPTEST library's C testing framework.
+// Contains initial definitions for the Runner, Section, Scenario and
+// prototype functions for handling each.
+//
+// Created	: 26 March 2025
+// Author	: Michael Pingleton
+//-----------------------------------------------------------------------------
+
+#ifndef CPPTEST_H
+#define CPPTEST_H
+
+#include <stdio.h>
+
+struct CTestRunner
+{};
+
+struct CTestSection
+{};
+
+struct CTestExpectationResult
+{};
+
+struct CTestScenario
+{};
+
+// TODO: expectation
+
+#endif
--- include/cpptest.hpp
+++ include/cpptest.hpp
@@ -3,9 +3,8 @@
 //
 // cpptest.hpp
 //
-// Primary inlcude header file for the CPPTEST library.
-// Contains initial defintions for the Runner, Suite, Case,
-// and Assertion classes.
+// Primary header file for the CPPTEST library's C++ testing framework.
+// Contains initial definitions for the Runner, Section, Scenario.
 //
 // Created	: 18 November 2024
 // Author	: Michael Pingleton
@@ -21,68 +20,17 @@ namespace cpptest
 {
 	using namespace std;
 
-	class Assertion
-	{
-		string d;
-		string e, a;
-		bool p;
-
-	public:
-		Assertion();
-		~Assertion();
-
-		bool isComplete();
-		bool isPassing();
-		string toString(int columnWidth);
-	};
-
-	class Case
-	{
-		string d;
-		bool w;
-		vector<Assertion> vA;
-
-	protected:
-		void expect(string description, bool condition);
-
-	public:
-		Case();
-		Case(string description);
-		~Case();
-
-		void skip();
-		bool isPassing();
-		int numberTotalAssertions();
-		int numberFailingAssertions();
-		string toString(int columnWidth, bool verbose);
-	};
-
-	class Suite
-	{
-		vector<Case*> vpC;
-
-	protected:
-		void add(Case* pCase);
-
-	public:
-		Suite();
-		~Suite();
-
-		string toString(int columnWidth);
-	};
-
 	class Runner
-	{
-		vector<Suite> vS;
+	{};
 
-	public:
-		Runner();
-		~Runner();
+	class Section
+	{};
 
-		void run();
-		int numberTotalSuites();
-		string toString(int columnWidth, bool verbose);
-	};
+	class ExpectationResult
+	{};
+
+	class Scenario
+	{};
 };
 
 #endif
